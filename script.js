c8744c48060c48072bf7a2ad59e81971a31cf52f
Alexei <y9y9bej@yandex.ru>
2025-12-25 16:31:11 +0300

    adding an array of months and a general calendar renderer function

--- script.js
+++ script.js
@@ -2,12 +2,27 @@
 
 const WEEK_DAYS = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
 
+const MONTHS = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 
+                'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
 document.addEventListener('DOMContentLoaded', () => {
-    const weekDaysContainer = document.getElementById('week-days');
-    WEEK_DAYS.forEach((weekDay) => {
-        let day = document.createElement('div');
-        day.textContent = weekDay;
-        console.log(day);
-        weekDaysContainer.appendChild(day);
-    });
+    let year = new Date;
+    let month = new Date;
+    let day = new Date;
+    year = year.getFullYear();
+    month = month.getMonth();
+    day = day.getDate();
+    console.log(MONTHS[month], year, day);
+
+    function renderCalendar(year, month, day) {
+        const weekDaysContainer = document.getElementById('week-days');
+        WEEK_DAYS.forEach((weekDay) => {
+            let day = document.createElement('div');
+            day.textContent = weekDay;
+            console.log(day);
+            weekDaysContainer.appendChild(day);
+        });
+    }
+    
+    renderCalendar(year, month, day);
 });
