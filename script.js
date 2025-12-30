eb941b38a2f3318673ad410f9cea107f2450fe8b
Alexei <y9y9bej@yandex.ru>
2025-12-30 08:29:14 +0300

    basic day markup and logic

--- script.js
+++ script.js
@@ -5,24 +5,48 @@ const WEEK_DAYS = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
 const MONTHS = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 
                 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
+function getFullDaysInMonth(year, month){
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    return daysInMonth;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let year = new Date;
     let month = new Date;
     let day = new Date;
-    year = year.getFullYear();
-    month = month.getMonth();
-    day = day.getDate();
-    console.log(MONTHS[month], year, day);
+    currentYear = year.getFullYear();
+    console.log(currentYear);
+    currentMonth = month.getMonth();
+    console.log(currentMonth);
+    currentDay = day.getDate();
+    console.log(currentDay);
 
     function renderCalendar(year, month, day) {
+
+        const daysInMonth = getFullDaysInMonth(year, month);
+
+        console.log(daysInMonth);
+
+        const firstDayInMonth = new Date(year, month, 1 - ((new Date(year, month, 1).getDay() + 6) % 7));
+
+        console.log(firstDayInMonth.getDate());
+
         const weekDaysContainer = document.getElementById('week-days');
         WEEK_DAYS.forEach((weekDay) => {
             let day = document.createElement('div');
             day.textContent = weekDay;
-            console.log(day);
             weekDaysContainer.appendChild(day);
         });
+
+        const daysContainer = document.getElementById('days-grid');
+        console.log(daysContainer);
+        for(let i = 0; i < 42; i++) {
+            let day = document.createElement('div');
+            day.textContent = 0;
+            daysContainer.appendChild(day);
+        }
+
     }
     
-    renderCalendar(year, month, day);
+    renderCalendar(currentYear, currentMonth, currentDay);
 });
